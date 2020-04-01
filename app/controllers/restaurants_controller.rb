8073b8cbf5beeaab26e35f37b9e9b2f73dfaafd8
Dave Winniford <dwinniford@gmail.com>
2020-04-01 17:01:21 -0500

    adds restaurant create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,6 +9,20 @@ class RestaurantsController < ApplicationController
     end
 
     def create 
+        restaurant = Restaurant.new(restaurant_params)
+        if restaurant.save 
+            redirect_to restaurant_path(restaurant)
+        else 
+            render '/restaurants/new'
+        end
+    end
 
+    def show 
+
+    end
+
+    private 
+    def restaurant_params 
+        params.require(:restaurant).permit(:name_chinese, :name_pinyin, :name_english, :country, :province, :city, :district, :street)
     end
 end
