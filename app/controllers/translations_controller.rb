dd545062fefee81a524f6d0f0748777a48af7753
Dave Winniford <dwinniford@gmail.com>
2020-04-02 15:54:56 -0500

    translation create method

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,10 +1,26 @@
 class TranslationsController < ApplicationController
+    before_action :set_restaurant, only: [:new, :create]
+    
     def new 
-        @restaurant = Restaurant.find(params[:restaurant_id])
         @translation = @restaurant.translations.build 
     end
 
     def create 
+        @translation = @restaurant.translations.build(translation_params)
+        if @translation.save 
+            redirect_to restaurant_translation_path(@translation)
+        else 
+            render :new 
+        end
+    end
 
+    private 
+
+    def set_restaurant 
+        @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+
+    def translation_params 
+        params.require(:translation).permit(:title, :notes, :language_id)
     end
 end
