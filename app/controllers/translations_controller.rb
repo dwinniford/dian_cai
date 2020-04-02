248602eeedba2f42a626a16a106d93112237746d
Dave Winniford <dwinniford@gmail.com>
2020-04-02 15:42:28 -0500

    new translation form

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,7 +1,7 @@
 class TranslationsController < ApplicationController
     def new 
         @restaurant = Restaurant.find(params[:restaurant_id])
-        
+        @translation = @restaurant.translations.build 
     end
 
     def create 
