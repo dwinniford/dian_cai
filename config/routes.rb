0089086ec38a6724c5ab9d10db6add7b6db345d4
Dave Winniford <dwinniford@gmail.com>
2020-04-02 15:22:48 -0500

    nest resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :translations
-  resources :restaurants
+  
+  resources :restaurants do 
+    resources :translations
+  end
   root 'application#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
