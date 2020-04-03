df8533d15d08135f277266527c828d8110ace5b9
Dave Winniford <dwinniford@gmail.com>
2020-04-03 14:48:46 -0500

    create and update restaurant with cuisines

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cuisine.create(chinese_name: "东北", pinyin_name: "DongBei", english_name: "Northeastern")
+Cuisine.create(chinese_name: "粤菜", pinyin_name: "YueCai", english_name: "Cantonese")
+Cuisine.create(chinese_name: "川菜", pinyin_name: "ChuanCai", english_name: "Sichuan")
+Cuisine.create(chinese_name: "湘菜", pinyin_name: "XiangCai", english_name: "Hunan")
+Cuisine.create(chinese_name: "徽菜", pinyin_name: "HuiCai", english_name: "Anhui")
+Cuisine.create(chinese_name: "闽菜", pinyin_name: "MinCai", english_name: "Fujian")
+Cuisine.create(chinese_name: "苏菜", pinyin_name: "SuCai", english_name: "Jiangsu")
+Cuisine.create(chinese_name: "鲁菜", pinyin_name: "LuCai", english_name: "Shandong")
+Cuisine.create(chinese_name: "浙菜", pinyin_name: "ZheCai", english_name: "Zhejiang")
