0272a07a3bf7bc03cfd85f113f9de2e3be413252
Dave Winniford <dwinniford@gmail.com>
2020-04-03 17:04:00 -0500

    user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   
-  resources :users
+  resources :users, only: [:new, :create, :show]
+
   resources :restaurants do 
     resources :translations, only: [:new, :create]
   end
