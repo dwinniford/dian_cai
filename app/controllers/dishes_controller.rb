55dc4c2a676a1bb8bf5b4840d36d4a0c73439600
Dave Winniford <dwinniford@gmail.com>
2020-04-03 08:48:46 -0500

    adds form for new dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,7 +1,7 @@
 class DishesController < ApplicationController
 
     def new 
-        @dish = Dish.new 
+        @dish = Dish.new # change to build off translation?
         @translation = Translation.find(params[:translation_id])
     end
 
