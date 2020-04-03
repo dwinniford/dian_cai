2b1460898d8080dff414977678cab955078082d8
Dave Winniford <dwinniford@gmail.com>
2020-04-03 07:32:39 -0500

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   
-  resources :dishes
+  
   resources :restaurants do 
     resources :translations, only: [:new, :create]
   end
 
-  resources :translations, only: [:show, :edit, :update, :destroy]
+  resources :translations, only: [:show, :edit, :update, :destroy] do 
+    resources :dishes, only: [:new, :create]
+  end
+
+  resources :dishes, only: [:show, :edit, :update, :destroy]
   
   root 'application#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
