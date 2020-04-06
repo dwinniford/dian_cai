6fe262ea9ce37e4120d73b3af4a330b330915027
Dave Winniford <dwinniford@gmail.com>
2020-04-06 07:56:15 -0500

    adds login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     end
 
     def show 
-        @user = User.find(params[:id])
+        @user = User.find(session[:user_id])
     end
 
     private 
