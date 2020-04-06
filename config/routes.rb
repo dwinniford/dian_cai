4776cddc3aab577fe9c9c20a220b2c6649f49579
Dave Winniford <dwinniford@gmail.com>
2020-04-06 07:38:50 -0500

    session routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   
   resources :users, only: [:new, :create, :show]
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/sessions', to: 'sessions#destroy'
+
   resources :restaurants do 
     resources :translations, only: [:new, :create]
   end
