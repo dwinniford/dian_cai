e9e6795080cea3c849e2a760cae54788d5d73340
Dave Winniford <dwinniford@gmail.com>
2020-04-06 07:21:54 -0500

    user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
     end
 
     def create 
-        
+        @user = User.new(user_params)
+        if @user.save 
+            redirect_to user_path(@user)
+        else 
+            render :new
+        end
+    end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
