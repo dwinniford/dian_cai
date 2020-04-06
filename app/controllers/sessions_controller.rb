c9a759e713a011c56fc48807177c6e6933474f27
Dave Winniford <dwinniford@gmail.com>
2020-04-06 17:24:02 -0500

    adds facebook create in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
         @user = User.new 
     end
 
-    def create 
+    def facebook
         @user = User.find_or_create_by(uid: auth['uid']) do |u|
             u.name = auth['info']['name']
             u.email = auth['info']['email']
@@ -13,13 +13,16 @@ class SessionsController < ApplicationController
         
         session[:user_id] = @user.id 
         redirect_to user_path(@user)
-        # @user = User.find_by(name: params[:user][:name])
-        # if @user && @user.authenticate(params[:user][:password])
-        #     session[:user_id] = @user.id 
-        #     redirect_to user_path(@user)
-        # else 
-        #     render :new
-        # end
+    end
+
+    def create 
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            render :new
+        end
     end
 
     def destroy 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
     has_many :translations 
     has_many :restaurants 
-    # has_secure_password 
-    # validates :password, confirmation: true 
-    # validates :password_confirmation, presence: true 
+    # has_secure_password if: not_using_omniauth?
+    # validates :password, confirmation: true, if: not_using_omniauth? 
+    # validates :password_confirmation, presence: true, if: not_using_omniauth? 
+    
+    
+    def not_using_omniauth? 
+        self.uid == nil 
+    end
 end
