4ded3ce01139c919ccc832bf7d0a8297b488df4c
Dave Winniford <dwinniford@gmail.com>
2020-04-07 16:17:10 -0500

    order new page with form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,11 @@
 class OrdersController < ApplicationController
+
+    def new 
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        @order = @restaurant.orders.build 
+    end
+
+    def create 
+
+    end
 end
