cb5d0d60b993c6c6335d568a6f6d2ed8edd69c6f
Dave Winniford <dwinniford@gmail.com>
2020-04-08 14:43:09 -0500

    dish validates presence of name

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,5 @@ class Dish < ApplicationRecord
     belongs_to :restaurant 
     has_many :dish_orders 
     has_many :orders, through: :dish_orders 
+    validates :chinese_name, :translated_name, presence: true 
 end
