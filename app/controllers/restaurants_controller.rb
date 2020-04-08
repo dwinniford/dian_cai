590954dce07f3928c76dd43c9d29f4f2d81ccca9
Dave Winniford <dwinniford@gmail.com>
2020-04-08 13:16:06 -0500

    restaurant is created with user

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,7 +12,7 @@ class RestaurantsController < ApplicationController
     end
 
     def create 
-        @restaurant = Restaurant.new(restaurant_params)
+        @restaurant = current_user.restaurants.build(restaurant_params)
         if @restaurant.save
             redirect_to restaurant_path(@restaurant), notice: "Restaurant was successfully created."
         else 
