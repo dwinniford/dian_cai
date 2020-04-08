9d27f0c5563b3534ab017061c688928ad985ba7c
Dave Winniford <dwinniford@gmail.com>
2020-04-08 15:04:01 -0500

    order validates presence

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
     belongs_to :restaurant 
     has_many :dish_orders 
     has_many :dishes, through: :dish_orders
+    validates :name, :people, :dishes, presence: true 
 end
