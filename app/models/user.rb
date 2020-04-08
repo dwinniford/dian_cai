a06270bbb59bfe474fe563c28cff25c7900d2fb7
Dave Winniford <dwinniford@gmail.com>
2020-04-08 13:50:11 -0500

    changes new user errors css

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
     has_many :orders 
     has_many :comments 
     has_secure_password 
+    validates :name, presence: true 
+    validates :email, presence: true 
     # validates :password, confirmation: true 
     # validates :password_confirmation, presence: true
     
