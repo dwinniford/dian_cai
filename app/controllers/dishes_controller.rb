6434eeddc78415569c524abf331984b1fff6c0fe
Dave Winniford <dwinniford@gmail.com>
2020-04-08 12:30:07 -0500

    dish controller checks user auth

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,7 +2,8 @@ class DishesController < ApplicationController
     before_action :set_translation, only: [:new, :create]
     before_action :set_dish, only: [:show, :edit, :update, :destroy]
     skip_before_action :check_if_logged_in, only: [:show]
-    
+    before_action :user_has_permission?, only: [:new, :create, :edit, :update, :destroy]
+
     def new 
         @dish = Dish.new # change to build off translation?
     end
@@ -56,5 +57,17 @@ class DishesController < ApplicationController
 
     def dish_params 
         params.require(:dish).permit(:chinese_name, :pinyin_name, :translated_name, :description, :ingredients, :spicy_level, :price, :calories, :restaurant_id)
+    end
+
+    def user_has_permission? 
+        if @translation 
+            unless current_user == @translation.user 
+                redirect_to translation_path(@translation), alert: "You do not have permission to perform this action."
+            end
+        else 
+            unless current_user == @dish.translation.user 
+                redirect_to dish_path(@dish), alert: "You do not have permission to perform this action."
+            end
+        end
     end
 end
