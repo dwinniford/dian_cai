a153806bf77214e435712f9f806088cd031f435f
Dave Winniford <dwinniford@gmail.com>
2020-04-08 08:52:30 -0500

    adds create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,27 @@
 class CommentsController < ApplicationController
-
+    before_action :set_translation, only: [:new, :create]
     def new 
-        @translation = Translation.find(params[:translation_id])
+        
         @comment = @translation.comments.build
+    end
+
+    def create 
+        @comment = @translation.comments.build(comment_params)
+        @comment.user = current_user
+        if @comment.save 
+            redirect_to translation_path(@translation)
+        else 
+            render :new 
+        end
+
+    end
+
+    private 
+    def set_translation 
+        @translation = Translation.find(params[:translation_id])
+    end
+    
+    def comment_params 
+        params.require(:comment).permit(:title, :content, :rating)
     end
 end
