a09bd8e81407861673502bb2764c8e13fc1d4dc1
Dave Winniford <dwinniford@gmail.com>
2020-04-08 07:43:42 -0500

    comment new page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+
+    def new 
+        @translation = Translation.find(params[:translation_id])
+        @comment = @translation.comments.build
+    end
 end
