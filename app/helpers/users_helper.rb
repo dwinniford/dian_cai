1fe09c541e748144b84c8f3950823911e51f9c29
Dave Winniford <dwinniford@gmail.com>
2020-04-09 14:32:32 -0500

    restaurant and translation conditional display

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,12 @@ module UsersHelper
     def current_user
         User.find(session[:user_id])
     end
+
+    def logged_in? 
+        !!session[:user_id]
+    end
+
+    def user_has_permission?(o)
+        logged_in? && current_user == o.user
+    end 
 end
