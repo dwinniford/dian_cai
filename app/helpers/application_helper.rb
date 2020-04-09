82ec10916762e4c96870dd5c11330eef77bc2672
Dave Winniford <dwinniford@gmail.com>
2020-04-09 09:07:23 -0500

    adds restaurant show partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def all_names(o) 
         "#{o.chinese_name} - #{o.pinyin_name} - #{o.translated_name}"
     end
+
+    def non_chinese_names(o)
+        "#{o.pinyin_name} - #{o.translated_name}"
+    end
 end
