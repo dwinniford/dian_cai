e0518fe8409760c7dcdff33b22e62e132e9744df
Dave Winniford <dwinniford@gmail.com>
2020-04-09 16:15:25 -0500

    adds translation average rating method

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -5,4 +5,14 @@ class Translation < ApplicationRecord
     belongs_to :user 
     has_many :comments 
     validates :title, presence: true 
+
+    def average_rating 
+        if self.comments.count > 0
+            self.comments.map { |c| c.rating }.sum/self.comments.count
+        else 
+            0
+        end 
+    end
+
+
 end
