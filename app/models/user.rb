6b0a2d343e92f9e51ce6c8bd24e43a8b04d476a9
Dave Winniford <dwinniford@gmail.com>
2020-04-10 10:26:50 -0500

    user show displays translations and orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,13 @@ class User < ApplicationRecord
     # validates :password, confirmation: true 
     # validates :password_confirmation, presence: true
     
+    def top_rated_translations(n) 
+        self.translations.sort_by { |t| t.average_rating }.reverse.first(n)
+    end
+
+    def most_recent_orders(n)
+        Order.where("user_id = ?", self.id).order(created_at: :desc).first(n)
+    end
     
    
 end
