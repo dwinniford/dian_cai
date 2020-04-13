fda207226f98f02f8434f590aa67f526b7284f56
Dave Winniford <dwinniford@gmail.com>
2020-04-13 10:01:15 -0500

    order sorts by people

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,8 @@ class Order < ApplicationRecord
     has_many :dishes, through: :dish_orders
     validates :name, presence: true  
     accepts_nested_attributes_for :dish_orders
+
+    def self.sort_by_people(restaurant)
+        Order.where("restaurant_id = ?", restaurant.id ).order(:people)
+    end
 end
