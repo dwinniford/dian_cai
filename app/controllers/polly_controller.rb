a929dd028ad0150d0f5b2e74fcdff7b6b9f55576
Dave Winniford <dwinniford@gmail.com>
2020-04-17 16:36:30 -0500

    attempt fix amazon polly

--- app/controllers/polly_controller.rb
+++ app/controllers/polly_controller.rb
@@ -9,12 +9,16 @@ class PollyController < ApplicationController
             text: params[:text],
             voice_id: "Zhiyu",
         })
-        # return resp.audio_stream
-        # response.headers['Content-Type'] = "mp3"
-        # response.stream.write resp.audio_stream
+       
+        # response.headers['Content-Type'] = "mp3/event-stream"
+        # response.stream.write  resp.audio_stream
         # response.stream.close
+        # rails error: undefined method `empty?' for #<StringIO:0x00007fbc8c3ef198>
+       
+        # send_data resp.audio_stream, filename: "#{params[:text]}.mp3", disposition: 'attachment'
+        #terminal: sent data (filename) browser console: failed to load because no supported source was found
+
         IO.copy_stream(resp.audio_stream, "#{params[:text]}.mp3") # successfully creates audio file in app folder
         send_file "#{params[:text]}.mp3"
-        # send_data resp.audio_stream, type: "mp3"
     end
 end
