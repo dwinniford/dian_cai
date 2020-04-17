3dc2d25143b99f3e27e8add7c73fddea87a6a5f1
Dave Winniford <dwinniford@gmail.com>
2020-04-17 10:40:57 -0500

    adds restaurant_cuisines helper method

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -3,6 +3,12 @@ module RestaurantsHelper
     def full_address(r)
         "#{r.country}, #{r.province}, #{r.city}, #{r.district}, #{r.street}"
     end
+
+    def restaurant_cuisines(r)
+        r.cuisines.map { |c| c.english_name }.join(", ")  
+    end
+
+    
     
     
 end
