de04a8664752f3c2c0b0396411fe0b412d801dd7
Dave Winniford <dwinniford@gmail.com>
2020-07-02 12:36:42 -0400

    restaurant index displays at proper scale

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,7 +1,7 @@
 module RestaurantsHelper
 
     def full_address(r)
-        "#{r.country}, #{r.province}, #{r.city}, #{r.district}, #{r.street}"
+        "#{r.country}, #{r.province}, #{r.city}, #{r.district},\n\n#{r.street}"
     end
 
     def restaurant_cuisines(r)
